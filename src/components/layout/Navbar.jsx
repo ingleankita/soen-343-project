8f95e03a9295bb0785c0b8110f904a26acd0673c
Ankita Ingle <aningle31@gmail.com>
2022-12-02 17:47:38 -0500

    Adding styling to Signup

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,36 +3,43 @@ import React from 'react';
 const Navbar = () => {
 	return (
 		<nav class='navbar navbar-expand-md'>
-			<a class='navbar-brand' href='#'>
-				<image src='' />
-			</a>
-			<ul class='navbar-nav mr-auto'>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>
-						ABOUT US
-					</a>
-				</li>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>
-						SERVICES
-					</a>
-				</li>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>
-						EVENTS
-					</a>
-				</li>
-				<li class='nav-item'>
-					<a class='nav-link' href='#'>
-						VOLUNTEER
-					</a>
-				</li>
-				<li class='nav-item active'>
-					<a class='nav-link' href='#'>
-						LOGIN/SIGNUP
-					</a>
-				</li>
-			</ul>
+			<div class='nav'>
+				<a class='navbar-brand' href='#'>
+					<img
+						src='/docs/4.0/assets/brand/bootstrap-solid.svg'
+						width='30'
+						height='30'
+						alt=''
+					/>
+				</a>
+				<ul class='navbar-nav mr-auto'>
+					<li class='nav-item'>
+						<a class='nav-link' href='#'>
+							ABOUT US
+						</a>
+					</li>
+					<li class='nav-item'>
+						<a class='nav-link' href='#'>
+							SERVICES
+						</a>
+					</li>
+					<li class='nav-item'>
+						<a class='nav-link' href='#'>
+							EVENTS
+						</a>
+					</li>
+					<li class='nav-item'>
+						<a class='nav-link' href='#'>
+							VOLUNTEER
+						</a>
+					</li>
+					<li class='nav-item active'>
+						<a class='nav-link' href='#'>
+							LOGIN/SIGNUP
+						</a>
+					</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
