268307a884dc7bdab53cbac05953f0dd7207a87f
Ankita Ingle <aningle31@gmail.com>
2022-12-02 17:36:31 -0500

    Adding styling to Signup

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<nav class='navbar navbar-expand-md'>
+			<a class='navbar-brand' href='#'>
+				<image src='' />
+			</a>
+			<ul class='navbar-nav mr-auto'>
+				<li class='nav-item'>
+					<a class='nav-link' href='#'>
+						ABOUT US
+					</a>
+				</li>
+				<li class='nav-item'>
+					<a class='nav-link' href='#'>
+						SERVICES
+					</a>
+				</li>
+				<li class='nav-item'>
+					<a class='nav-link' href='#'>
+						EVENTS
+					</a>
+				</li>
+				<li class='nav-item'>
+					<a class='nav-link' href='#'>
+						VOLUNTEER
+					</a>
+				</li>
+				<li class='nav-item active'>
+					<a class='nav-link' href='#'>
+						LOGIN/SIGNUP
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
