8238984391f699f55892801fde3755bdbc5d24bd
Ankita Ingle <aningle31@gmail.com>
2022-12-02 16:41:02 -0500

    Implement Signup and Welcome page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Signup from './components/Signup';
+import Welcome from './components/Welcome';
 
 function App() {
 	const [username, setUsername] = useState('');
 
-	return (
-		<div className='App'>
-			<Navbar />
-			<Signup setUsername={setUsername} />
-			<Footer />
-		</div>
-	);
+	if (username == '') {
+		return (
+			<div className='App'>
+				<Navbar />
+				<Signup setUsername={setUsername} />
+				<Footer />
+			</div>
+		);
+	} else {
+		return <Welcome username={username} />;
+	}
 }
 
 export default App;
